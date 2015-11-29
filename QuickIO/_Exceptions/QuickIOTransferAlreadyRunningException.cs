8b0524fa738497f1ede0bfb07a4e619648ed769f
Benjamin Abt <mail@benjamin-abt.com>
2015-11-29 18:36:16 +0100

    Exceptions moved to directory to cleanup solution

---
+++ QuickIO/_Exceptions/QuickIOTransferAlreadyRunningException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchwabenCode.QuickIO
+{
+    public class QuickIOTransferAlreadyRunningException : Exception
+    {
+        public QuickIOTransferAlreadyRunningException( string message ) : base( message )
+        {
+
+        }
+    }
+}
