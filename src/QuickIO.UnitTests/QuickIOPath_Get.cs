759fb48f778b4fb3098e60a0ac8601005f9c7e6d
BenjaminAbt <mail@benjamin-abt.com>
2016-01-18 10:50:24 +0100

    Added GetPathRoot based on string functions

--- src/QuickIO.UnitTests/QuickIOPath_Get.cs
+++ src/QuickIO.UnitTests/QuickIOPath_Get.cs
@@ -6,6 +6,35 @@ namespace SchwabenCode.QuickIO.UnitTests
 
     public class QuickIOPath_Get
     {
+
+        [Theory]
+        [InlineData( null, null )]
+        [InlineData( "", "" )]
+        [InlineData( @"folder\file", "" )]
+        [InlineData( @"C:\", @"C:\" )]
+        [InlineData( @"C:\folder", @"C:\" )]
+        [InlineData( @"C:\file.txt", @"C:\" )]
+        [InlineData( @"C:\folder\file.txt", @"C:\" )]
+        [InlineData( @"\\server\share", @"\\server\share" )]
+        [InlineData( @"\\server\share\", @"\\server\share" )]
+        [InlineData( @"\\server\share\folder", @"\\server\share" )]
+        [InlineData( @"\\server\share\file.txt", @"\\server\share" )]
+        [InlineData( @"\\server\share\folder\file.txt", @"\\server\share" )]
+        [InlineData( @"\\?\C:\", @"\\?\C:\" )]
+        [InlineData( @"\\?\C:\folder", @"\\?\C:\" )]
+        [InlineData( @"\\?\C:\file.txt", @"\\?\C:\" )]
+        [InlineData( @"\\?\C:\folder\file.txt", @"\\?\C:\" )]
+        [InlineData( @"\\?\UNC\server\share", @"\\?\UNC\server\share" )]
+        [InlineData( @"\\?\UNC\server\share\", @"\\?\UNC\server\share" )]
+        [InlineData( @"\\?\UNC\server\share\folder", @"\\?\UNC\server\share" )]
+        [InlineData( @"\\?\UNC\server\share\file.txt", @"\\?\UNC\server\share" )]
+        [InlineData( @"\\?\UNC\server\share\folder\file.txt", @"\\?\UNC\server\share" )]
+        public void GetPathRoot( string test, string expected )
+        {
+            QuickIOPath.GetPathRoot( test ).Should().Be( expected );
+        }
+
+
         [Theory]
         [InlineData( @"C:\", null )]
         [InlineData( "\\server", null )]
