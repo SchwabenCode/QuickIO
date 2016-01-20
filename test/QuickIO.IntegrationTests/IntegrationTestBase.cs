60ee3cf46d2fb3b7e9f5fc552a1f309290746035
BenjaminAbt <mail@benjamin-abt.com>
2016-01-20 10:54:30 +0100

    Fixed current path determination

---
+++ test/QuickIO.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SchwabenCode.QuickIO.IntegrationTests
+{
+    public abstract class IntegrationTestBase
+    {
+        public static string CurrentPath()
+        {
+            return Environment.CurrentDirectory;
+        }
+    }
+}
---
+++ test/QuickIO.UnitTests/UnitTestBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace SchwabenCode.QuickIO.UnitTests
+{
+    public abstract class UnitTestBase
+    {
+        public static string CurrentPath()
+        {
+            return Environment.CurrentDirectory;
+        }
+    }
+}
