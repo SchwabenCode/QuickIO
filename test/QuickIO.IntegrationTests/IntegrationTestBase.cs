b5e7d1425d9fabee9b486704c1a4a6c1fa22e866
BenjaminAbt <mail@benjamin-abt.com>
2016-01-20 12:14:07 +0100

    Removed PlatformExtensions and added System.AppContext.BaseDirectory

--- test/QuickIO.IntegrationTests/IntegrationTestBase.cs
+++ test/QuickIO.IntegrationTests/IntegrationTestBase.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.Extensions.PlatformAbstractions;
-
-namespace SchwabenCode.QuickIO.IntegrationTests
+﻿namespace SchwabenCode.QuickIO.IntegrationTests
 {
     public abstract class IntegrationTestBase
     {
         public static string CurrentPath()
         {
-            return PlatformServices.Default.Application.ApplicationBasePath;
+            return System.AppContext.BaseDirectory;
         }
     }
 }
