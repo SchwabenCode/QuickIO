87740590a5632c52016844c0d842fc227bfb5b0d
BenjaminAbt <mail@benjamin-abt.com>
2016-01-20 10:54:56 +0100

    because of the test system we have to work with full paths

--- test/QuickIO.IntegrationTests/QuickIODirectoryInfo_Enumerations.cs
+++ test/QuickIO.IntegrationTests/QuickIODirectoryInfo_Enumerations.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
 
 namespace SchwabenCode.QuickIO.IntegrationTests
 {
-    public class QuickIODirectoryInfo_Enumerations
+    public class QuickIODirectoryInfo_Enumerations : IntegrationTestBase
     {
         [Theory]
-        [InlineData( "_TestFolders/ExistingFolder", "*.txt", 1 )]
+        [InlineData( true, "_TestFolders/ExistingFolder", "*.txt", 1 )]
         //[InlineData( "_TestFolders/ExistingFolder", "*.txt", 1 )]
         //[InlineData( "_TestFolders/ExistingFolder", "*.TXT", 1 )]
-        public void QuickIODirectoryInfo_EnumerateFilesCount( string path, string pattern, int expected )
+        public void QuickIODirectoryInfo_EnumerateFilesCount( bool isRelative, string path, string pattern, int expected )
         {
+            if( isRelative )
+            {
+                path = QuickIOPath.Combine( CurrentPath(), path );
+            }
+
             QuickIODirectoryInfo directoryInfo = new QuickIODirectoryInfo( path );
             IEnumerable<QuickIOFileInfo> result = ( pattern == null ? directoryInfo.EnumerateFiles() : directoryInfo.EnumerateFiles( pattern ) );
 
