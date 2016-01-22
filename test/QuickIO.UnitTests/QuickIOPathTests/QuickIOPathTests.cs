dd486b15fd1a09a7ba92290bce217f6f404ce257
Benjamin Abt <mail@benjamin-abt.com>
2016-01-22 16:26:09 +0100

    new tests for path combine added

---
+++ test/QuickIO.UnitTests/QuickIOPathTests/QuickIOPathTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using FluentAssertions;
+using Xunit;
+
+namespace SchwabenCode.QuickIO.UnitTests.QuickIOPathTests
+{
+    public class QuickIOPathTests
+    {
+        [Theory]
+        [InlineData( "folder1", "folder2" )]
+        [InlineData( @"C:\folder1", "folder2" )]
+        [InlineData( @"\\server\share\folder1", "folder2" )]
+        public void Combine( string p1, string p2 )
+        {
+            QuickIOPath.Combine( p1, p2 ).Should().Be( System.IO.Path.Combine( p1, p2 ) );
+        }
+    }
+}
