cd3342f9499765a80b2bf2d9e59d7eb9ba21ff8b
Benjamin Abt <mail@benjamin-abt.com>
2016-01-24 22:45:37 +0100

    Added tests

--- test/QuickIO.UnitTests/InternalHelpersTests.cs
+++ test/QuickIO.UnitTests/InternalHelpersTests.cs
@@ -18,17 +18,40 @@ namespace SchwabenCode.QuickIO.UnitTests
 
             FileAttributes result = InternalHelpers.AddFileAttrribute( attr, FileAttributes.System );
 
+            InternalHelpers.ContainsFileAttribute( result, FileAttributes.Archive ).Should().Be( true );
             InternalHelpers.ContainsFileAttribute( result, FileAttributes.System ).Should().Be( true );
+        }
+
+        [Fact]
+        public void TryAddFileAttrribute()
+        {
+            FileAttributes attr = FileAttributes.Archive;
+
+            InternalHelpers.TryAddFileAttrribute( attr, FileAttributes.System, out attr ).Should().BeTrue( "First add failed" );
+            InternalHelpers.ContainsFileAttribute( attr, FileAttributes.System ).Should().Be( true, "First test failed" );
+
+            InternalHelpers.TryAddFileAttrribute( attr, FileAttributes.System, out attr ).Should().BeFalse( "Second add failed" );
+            InternalHelpers.ContainsFileAttribute( attr, FileAttributes.System ).Should().Be( true, "Second test failed" );
+        }
+
+        [Fact]
+        public void TryRemoveFileAttrribute()
+        {
+            FileAttributes attr = FileAttributes.Archive | FileAttributes.System;
+
+            InternalHelpers.TryRemoveFileAttrribute( attr, FileAttributes.System, out attr ).Should().BeTrue( "First remove failed" );
+            InternalHelpers.ContainsFileAttribute( attr, FileAttributes.System ).Should().Be( false, "First test failed" );
         }
 
         [Fact]
         public void RemoveFileAttribute()
         {
-            FileAttributes attr = FileAttributes.Archive | FileAttributes.Normal;
+            FileAttributes attr = FileAttributes.Archive | FileAttributes.System;
 
             FileAttributes result = InternalHelpers.RemoveFileAttribute( attr, FileAttributes.Archive );
 
             InternalHelpers.ContainsFileAttribute( result, FileAttributes.Archive ).Should().Be( false );
+            InternalHelpers.ContainsFileAttribute( result, FileAttributes.System ).Should().Be( true );
         }
 
         [Fact]
