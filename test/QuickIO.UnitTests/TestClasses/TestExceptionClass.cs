2ac7cc3bde0f961ab909dde06787aa82a7d5417b
Benjamin Abt <mail@benjamin-abt.com>
2016-01-24 21:31:37 +0100

    Added test class for abstract exception class of
    QuickIO

---
+++ test/QuickIO.UnitTests/TestClasses/TestExceptionClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SchwabenCode.QuickIO.Core;
+
+namespace SchwabenCode.QuickIO.UnitTests.TestClasses
+{
+    public class TestExceptionClass : QuickIOBaseException
+    {
+        public TestExceptionClass( string message, string path ) : base( message, path )
+        {
+        }
+
+        public TestExceptionClass( string message, string path, Exception innerException ) : base( message, path, innerException )
+        {
+        }
+    }
+}
