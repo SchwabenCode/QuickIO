8f535a224e666c56debcbf84f5d394d0320bd235
Benjamin Abt <mail@benjamin-abt.com>
2016-01-24 20:54:19 +0100

    Added assemblyinfo

--- src/QuickIO.NET/Properties/AssemblyInfo.cs
+++ src/QuickIO.NET/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo( "SchwabenCode.QuickIO.UnitTests" )]
+[assembly: InternalsVisibleTo( "QuickIO.UnitTests" )]
 [assembly: AssemblyMetadata("Serviceable", "True")]
 [assembly: NeutralResourcesLanguage("en-US")]
