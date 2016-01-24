02fb106622d108d5d0a4500dde0a8a9c4427862f
Benjamin Abt <mail@benjamin-abt.com>
2016-01-24 22:56:22 +0100

    Added tests for QuickIOFileSystemEntry

---
+++ test/QuickIO.UnitTests/QuickIOFileSystemEntryTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using FluentAssertions;
+using SchwabenCode.QuickIO.Core;
+using Xunit;
+
+namespace SchwabenCode.QuickIO.UnitTests
+{
+    public class QuickIOFileSystemEntryTests
+    {
+        [Fact]
+        public void QuickIOFileSystemEntryCtor()
+        {
+            QuickIOFileSystemEntry fse = new QuickIOFileSystemEntry( @"C:\temp\quickio",
+                QuickIOFileSystemEntryType.Directory, FileAttributes.Directory | FileAttributes.Hidden, 1024 );
+
+            fse.Should().NotBe( null );
+
+            fse.Path.Should().Be( @"C:\temp\quickio" );
+            fse.GetPathUnc().Should().Be( @"\\?\C:\temp\quickio" );
+            fse.GetPathRegular().Should().Be( @"C:\temp\quickio" );
+
+            fse.Type.Should().Be( QuickIOFileSystemEntryType.Directory );
+
+            InternalHelpers.ContainsFileAttribute( fse.Attributes, FileAttributes.Directory ).Should().BeTrue();
+            InternalHelpers.ContainsFileAttribute( fse.Attributes, FileAttributes.Hidden ).Should().BeTrue();
+            InternalHelpers.ContainsFileAttribute( fse.Attributes, FileAttributes.Encrypted ).Should().BeFalse();
+
+            fse.Bytes.Should().Be( 1024 );
+        }
+    }
+}
