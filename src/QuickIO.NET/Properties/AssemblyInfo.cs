43d2ef52b5ba24589f2694119eef3eb2c3797b0c
BenjaminAbt <mail@benjamin-abt.com>
2016-01-25 09:37:33 +0100

    Assemby visiblito to integration tests

--- src/QuickIO.NET/Properties/AssemblyInfo.cs
+++ src/QuickIO.NET/Properties/AssemblyInfo.cs
@@ -3,5 +3,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo( "QuickIO.UnitTests" )]
+[assembly: InternalsVisibleTo( "QuickIO.IntegrationTests" )]
 [assembly: AssemblyMetadata("Serviceable", "True")]
 [assembly: NeutralResourcesLanguage("en-US")]
