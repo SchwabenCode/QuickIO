eaf29c61d956df70eff8cb367e031f5afd02a33d
BenjaminAbt <mail@benjamin-abt.com>
2016-01-27 14:27:47 +0100

    Removed wrong patterns

--- src/QuickIO.NET/QuickIOPatterns.cs
+++ src/QuickIO.NET/QuickIOPatterns.cs
@@ -35,47 +35,5 @@ namespace SchwabenCode.QuickIO
         /// <remarks>([\da-fA-F]{1,4}(\:[\da-fA-F]{1,4}){7})|(([\da-fA-F]{1,4}:){0,5}::([\da-fA-F]{1,4}:){0,5}[\da-fA-F]{1,4})|(((([1]?\d)?\d|2[0-4]\d|25[0-5])\.){3}(([1]?\d)?\d|2[0-4]\d|25[0-5]))</remarks>
         public const String IPv4v6Pattern = IPv4Pattern + @"|" + IPv6Pattern;
 
-        /// <summary>
-        /// Matching Server Name
-        /// </summary>
-        /// <remarks>[a-zA-Z0-9_]+</remarks>
-        public const String ServerNamePattern = @"[a-zA-Z0-9_]+";
-        /// <summary>
-        /// Matching QuickIOShareInfo Name
-        /// </summary>
-        /// <remarks>[a-zA-Z0-9_]+</remarks>
-        public const String ShareNamePattern = @"[a-zA-Z0-9_]+";
-        /// <summary>
-        /// Matching Folder Name
-        /// </summary>
-        /// <remarks>[a-zA-Z0-9_]+</remarks>
-        public const String FolderNamePattern = @"[a-zA-Z0-9_]+";
-
-        /// <summary>
-        /// Optional UNC Separator
-        /// </summary>
-        /// <remarks>\\{0,1}</remarks>
-        public const String OptionalSperatorPattern = @"\\{0,1}";
-
-        /// <summary>
-        /// Pattern matching share root
-        /// </summary>
-        public const String RegularShareRootPattern = @"(?<Root>\\\\(?<Server>" + ServerNamePattern + @"|" + IPv4v6Pattern + @")\\(?<Name>" + ShareNamePattern + @"))" + OptionalSperatorPattern;
-
-        /// <summary>
-        /// Pattern matching share path
-        /// </summary>
-        public const String RegularSharePattern = @"(?<Fullname>" + RegularShareRootPattern + @"\\{0,1}(?<Path>((\\(" + FolderNamePattern + @")){0,})))" + OptionalSperatorPattern;
-
-        ///// <summary>
-        ///// Returns ^<paramref name="pattern"/>$
-        ///// </summary>
-        //public static String GetStrict( String pattern )
-        //{
-        //    Contract.Requires( pattern != null );
-        //    Contract.Ensures( Contract.Result<string>() != null );
-
-        //    return "^" + pattern + "$";
-        //}
     }
 }
