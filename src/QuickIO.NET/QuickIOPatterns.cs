e517ee4174b2650eb32501b6fbcb3bdf29045def
BenjaminAbt <mail@benjamin-abt.com>
2016-01-27 14:58:57 +0100

    Removed unused IP Patterns

--- src/QuickIO.NET/QuickIOPatterns.cs
+++ src/QuickIO.NET/QuickIOPatterns.cs
@@ -16,24 +16,5 @@ namespace SchwabenCode.QuickIO
         /// Matches all. No filtering.
         /// </summary>
         public const String PathMatchAll = "*";
-
-        /// <summary>
-        /// Pattern to match IPv6
-        /// </summary>
-        /// <remarks>([\da-fA-F]{1,4}(\:[\da-fA-F]{1,4}){7})|(([\da-fA-F]{1,4}:){0,5}::([\da-fA-F]{1,4}:){0,5}[\da-fA-F]{1,4})</remarks>
-        public const string IPv6Pattern = @"([\da-fA-F]{1,4}(\:[\da-fA-F]{1,4}){7})|(([\da-fA-F]{1,4}:){0,5}::([\da-fA-F]{1,4}:){0,5}[\da-fA-F]{1,4})";
-
-        /// <summary>
-        /// Pattern to match IPv4
-        /// </summary>
-        /// <remarks>(((([1]?\d)?\d|2[0-4]\d|25[0-5])\.){3}(([1]?\d)?\d|2[0-4]\d|25[0-5]))</remarks>
-        public const String IPv4Pattern = @"(((([1]?\d)?\d|2[0-4]\d|25[0-5])\.){3}(([1]?\d)?\d|2[0-4]\d|25[0-5]))";
-
-        /// <summary>
-        /// Pattern to match IPv4 and IPv6
-        /// </summary>
-        /// <remarks>([\da-fA-F]{1,4}(\:[\da-fA-F]{1,4}){7})|(([\da-fA-F]{1,4}:){0,5}::([\da-fA-F]{1,4}:){0,5}[\da-fA-F]{1,4})|(((([1]?\d)?\d|2[0-4]\d|25[0-5])\.){3}(([1]?\d)?\d|2[0-4]\d|25[0-5]))</remarks>
-        public const String IPv4v6Pattern = IPv4Pattern + @"|" + IPv6Pattern;
-
     }
 }
